40e6169e379d1f5e96faa61f1616fe390b74b5e9
Naman Yadav <raymannky2001@gmail.com>
2023-07-23 22:05:39 +0530

    added fucntionality of form and give some structure to the page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,102 @@
+import { useState } from "react";
 import "./index.css";
 
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 export default function App() {
-  return null;
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴Far Away👜</h1>;
+}
+function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setquantity] = useState(1);
+
+  function handlesubmit(e) {
+    e.preventDefault();
+
+    if (!description) {
+      return;
+    }
+    const newItem = {
+      description: description,
+      quantity: quantity,
+      id: Date.now(),
+      packed: false,
+    };
+    console.log(newItem);
+    setDescription("");
+    setquantity(1);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handlesubmit}>
+      <h3>What do you need for your 🏄 trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => {
+          return setquantity(Number(e.target.value));
+        }}
+      >
+        {Array.from({ length: 20 }, (_, i) => (i = i + 1)).map((num) => {
+          return (
+            <option value={num} key={num}>
+              {num}
+            </option>
+          );
+        })}
+      </select>
+      <input
+        type="text"
+        placeholder="item.."
+        value={description}
+        onChange={(e) => {
+          return setDescription(e.target.value);
+        }}
+      />
+      <button className="">ADD</button>
+    </form>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function PackingList() {
+  return (
+    <section className="list">
+      <ul>
+        {initialItems.map((item) => {
+          return <Item item={item} key={item.id} />;
+        })}
+      </ul>
+    </section>
+  );
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X items on your list, and you already packed X (X*)</em>
+    </footer>
+  );
 }
